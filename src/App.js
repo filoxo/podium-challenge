04f6b9e77a1336626999b2a72156229433fc7e8b
Carlos Filoteo <c.filoteo@live.com>
2017-08-26 23:04:27 -0600

    Use FetchResponse in App to display data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import FetchResponse from './api/FetchResponse';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>App</div>
+      <div>
+        <FetchResponse endpoint="http://shakespeare.podium.co/api/reviews">
+          {({fetching, data}) => (
+            fetching 
+              ? <div>Fetching data...</div> 
+              : <div>Has data: {JSON.stringify(data)}</div>
+          )}
+        </FetchResponse> 
+      </div>
     );
   }
 }
