fcb5c9eb1a296db46e344af02985ec71b1157f17
Carlos Filoteo <c.filoteo@live.com>
2017-08-28 08:13:37 -0600

    Style ReviewCards

--- src/component/ReviewCard.js
+++ src/component/ReviewCard.js
@@ -2,9 +2,13 @@ import React from 'react'
 import StarRating from './StarRating';
 
 const ReviewCard = ({id, author, rating, publish_date}) => 
-    <div>
+    <div style={{
+    	display: 'flex',
+	    margin: '3px',
+	    boxShadow: '0 1px 3px rgba(0,0,0,.3)'
+    }}>
         <StarRating rating={rating}/>
-        <div>
+        <div style={{marginLeft: '12px'}}>
             <h3>{author}</h3>
             <p>Published: {new Date(publish_date).toLocaleString()}</p>
         </div>
--- src/component/StarRating.js
+++ src/component/StarRating.js
@@ -1,6 +1,25 @@
 import React from 'react'
 
+function getRatingColor(value){
+    // rating from 0 to 5
+    // hsl range 10 to 45
+    // Inspired by https://dribbble.com/shots/3416731-Nimber-Decimal-rating
+    return `hsl(${value * 7 + 10}, 100%, 50%)`
+}
+
 const StarRating = ({rating}) =>
-    <span>{rating}&nbsp;★</span>
+    <span style={{
+		    alignItems: 'center',
+        backgroundColor: getRatingColor(rating),
+        color: '#fff',
+		    display: 'flex',
+		    flexDirection: 'column',
+        fontSize: '20px',
+        fontWeight: 'bold',
+		    justifyContent: 'center',
+	      minWidth: '55px',
+		    padding: '10px',
+        textShadow: 'rgba(0,0,0,.3) 0 0 2px, rgba(0,0,0,.3) 0 0 5px'
+    }}>{rating}&nbsp;★</span>
 
 export default StarRating
