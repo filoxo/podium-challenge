8b74d81846ac09d005f204325f068069a06dd79c
Carlos Filoteo <c.filoteo@live.com>
2017-08-28 07:07:33 -0600

    Shallow render FetchResponse with mock

--- src/api/FetchResponse.test.js
+++ src/api/FetchResponse.test.js
@@ -1,9 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import FetchResponse from './FetchResponse';
+import fetchMock from 'fetch-mock';
+
+beforeAll(() => {
+  fetchMock.get('*', {hello: 'world'});  
+})
 
 it('renders an element', () => {
   const div = document.createElement('div');
   const PASS = "PASS"
-//   ReactDOM.render(<FetchResponse>{() => <div>{PASS}</div>}</FetchResponse>, div);
+  ReactDOM.render(
+    <FetchResponse children={({fetching, data}) => {
+      if(fetching) {
+        expect(data).toBeNull()
+      } else {
+        expect(data).not.toBeNull()
+      }
+      return <div>{PASS}</div>
+    }}/>
+    , div);
 });
