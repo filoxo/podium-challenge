b05e45120cd5e878f338f8426a1cb23588532890
Carlos Filoteo <c.filoteo@live.com>
2017-08-28 21:46:26 -0600

    Add "see all" link to ReviewCard

--- src/component/ReviewCard.js
+++ src/component/ReviewCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import StarRating from './StarRating';
 
 const ReviewCard = ({id, author, rating, publish_date}) => 
@@ -8,9 +9,16 @@ const ReviewCard = ({id, author, rating, publish_date}) =>
 	    boxShadow: '0 1px 3px rgba(0,0,0,.3)'
     }}>
         <StarRating rating={rating}/>
-        <div style={{marginLeft: '12px'}}>
+        <div style={{
+        	marginLeft: '12px',
+	        flex: '1',
+	        padding: '6px 12px 6px 0'
+        }}>
             <h3>{author}</h3>
             <p>Published: {new Date(publish_date).toLocaleString()}</p>
+		        <div className="textRight">
+			        <Link to={`/review/${id}`}>See full review...</Link>
+		        </div>
         </div>
     </div>
 
