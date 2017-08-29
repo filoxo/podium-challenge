f7cc692bb55236b92983843323b0a49c9ff25a57
Carlos Filoteo <c.filoteo@live.com>
2017-08-28 21:38:00 -0600

    Remove main element from HomeRoute

--- src/routes/HomeRoute.js
+++ src/routes/HomeRoute.js
@@ -6,13 +6,13 @@ const HomeRoute = () => <FetchResponse endpoint="http://shakespeare.podium.co/ap
 	{({fetching, data}) => (
 		fetching
 			? <div className="textCenter">Fetching data...</div>
-			: <main style={{
+			: <div style={{
 				margin: '0 auto',
 				maxWidth: '400px',
 				padding: '10px 0'
 			}}>
 				{ data.map((review) => <ReviewCard key={review.id} {...review}/>) }
-			</main>
+			</div>
 	)}
 </FetchResponse>
 
